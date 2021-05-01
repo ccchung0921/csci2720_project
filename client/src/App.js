32fa9124dc0a0dec6d0b61df4ea82e152805dd42
anshul6244 <anshul.ghanshala@kosmic.io>
2021-05-01 17:10:59 +0800

    Login

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,27 @@ import MainPage from './screens/MainPage'
 import PlaceDetail from './screens/PlaceDetail'
 import NavbarComponent from './components/Navbar';
 import {BrowserRouter as Router, Route,Switch} from 'react-router-dom';
+import styled from "styled-components";
+import { AccountBox } from "./components/accountBox";
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
 
 function App() {
   return (
     <>
     <NavbarComponent />
     <Router>
+    <AppContainer>
+      <AccountBox />
+    </AppContainer>
       <Switch>
         <Route path='/place/:id' component={PlaceDetail} />
         <Route path='/' component={MainPage} />
